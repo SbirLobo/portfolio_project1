00f047d43fd02ba2eecaf0fa214bfb91581f2e81
Denis Dezest <denis.dezest@gmail.com>
2023-03-23 13:41:20 +0100

    update of the nav mobile menu: clicking on a nav item now allows to close the nav menu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const navList = document.getElementById("nav--list");
+const navListItems = document.querySelectorAll(".nav--item");
 const navMenu = document.querySelector(".nav--mobile-btn");
 
 const button = document.querySelector(".btnup");
@@ -15,9 +16,24 @@ let classRef = "";
 /* ____________________________________________ *\
     $MOBILE NAV MENU
 \* ____________________________________________ */
-navMenu.addEventListener("click", (event) => {
+const toggleNavMenu = function () {
   navMenu.classList.toggle("is-open");
   navList.classList.toggle("is-open");
+};
+
+navMenu.addEventListener("click", (event) => {
+  toggleNavMenu();
+});
+
+navListItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    if (
+      navMenu.classList.contains("is-open") ||
+      navList.classList.contains("is-open")
+    ) {
+      toggleNavMenu();
+    }
+  });
 });
 
 /* ____________________________________________ *\
